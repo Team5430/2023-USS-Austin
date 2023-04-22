a652061b2a7c03843bb42f8a68594316fdf101e9
angtroph <riomaneclang@gmail.com>
2023-04-21 23:26:49 -0700

    feat. gripper auto switchcase skeleton

--- src/main/java/frc/robot/commands/GripCommand.java
+++ src/main/java/frc/robot/commands/GripCommand.java
@@ -8,5 +8,31 @@ import frc.robot.subsystems.GripperSubsystem;
 
 public class GripCommand extends CommandBase {
     private final TalonSRX gTalonSRX = new TalonSRX(7);
-
+    public class Gripper{
+        private static gripperStrengthCube;
+        private static gripperStrengthCone;
+        public Gripper(boolean isGripping, String cubeOrCone){
+            switch (cubeOrCone){
+                case "Cube":
+                case "CUBE":
+                case "cube":
+                    System.out.println("Cube Selected");
+                    // change values to gripper strength cube
+                    break;
+                case "Cone":
+                case "CONE":
+                case "cone":
+                    System.out.println("Cone Selected");
+                    // change values of motor to gripper strength cone
+                    break;
+                default:
+                    System.out.println("Cube Selected");
+                    // change values to gripper strength cube
+                    break;
+            }
+            switch (isGripping){
+                
+            }
+        }
+    }
 }
