694871547b6fa33c32763445a5890dbde1bb91da
5430E <frc5430@gmail.com>
2023-05-15 16:21:09 -0700

    Co-authored-by: frc5430c <frc5430c@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/armSub.java
+++ src/main/java/frc/robot/subsystems/armSub.java
@@ -1,39 +1,12 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.*;
-
 import frc.robot.Constants;
-import frc.robot.Constants.CANid;;
 
 public class armSub extends SubsystemBase{
-    private TalonSRX rotateSRX = new TalonSRX(Constants.CANid.kRotateSRX);
     private TalonSRX extendSRX = new TalonSRX(Constants.CANid.kExtendSRX);
-    TalonSRXConfiguration configSRX = new TalonSRXConfiguration();
-
-public void SRXsettings(){
-//Settings
-configSRX.peakCurrentLimit = 40; //Peak amps before it reduces it
-configSRX.peakCurrentDuration = 1300; //Amount of time it allows for the Talon's to stay at peak amps(in miliseconds)
-    //sets settings to the Talons
-    rotateSRX.configAllSettings(configSRX);
-    extendSRX.configAllSettings(configSRX);
-
-}
-
-    public void rotating(double axis){
-        rotateSRX.set(ControlMode.PercentOutput, axis);
-    }
-
-    public void extending(double axis){
-        extendSRX.set(ControlMode.PercentOutput, axis);
-    }
-        
-     
     
 
 }
