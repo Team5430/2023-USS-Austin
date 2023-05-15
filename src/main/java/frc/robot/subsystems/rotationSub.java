4471492ef2f27b14e70bff1fc871750c35999ff0
5430E <frc5430@gmail.com>
2023-05-15 16:21:09 -0700

    Co-authored-by: frc5430c <frc5430c@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/rotationSub.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.*;
+
+import frc.robot.Constants;
+import frc.robot.Constants.CANid;;
+
+public class rotationSub extends SubsystemBase{
+    private TalonSRX rotateSRX = new TalonSRX(Constants.CANid.kRotateSRX);
+    TalonSRXConfiguration configSRX = new TalonSRXConfiguration();
+
+public void SRXsettings(){
+//Settings
+configSRX.peakCurrentLimit = 40; //Peak amps before it reduces it
+configSRX.peakCurrentDuration = 1300; //Amount of time it allows for the Talon's to stay at peak amps(in miliseconds)
+    //sets settings to the Talons
+    rotateSRX.configAllSettings(configSRX);
+
+}
+
+    public void rotating(double axis){
+        rotateSRX.set(ControlMode.PercentOutput, axis);
+    }
+
+  
+        
+     
+    
+
+}
