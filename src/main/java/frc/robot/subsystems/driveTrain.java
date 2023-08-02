75df3e6088cec88c27c656684a4d76a72b92d813
Anthony Saysipaseuth <nickki456@gmail.com>
2023-08-01 17:01:39 -0700

    A

--- src/main/java/frc/robot/subsystems/driveTrain.java
+++ src/main/java/frc/robot/subsystems/driveTrain.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 
 public class driveTrain extends SubsystemBase  {
 
-   final static  WPI_TalonFX backRightMotor = new WPI_TalonFX(Constants.CANid.kBackRightFX);//
+   final static  WPI_TalonFX backRightMotor = new WPI_TalonFX(Constants.CANid.kBackRightFX);
    final static  WPI_TalonFX backLeftMotor = new WPI_TalonFX(Constants.CANid.kBackLeftFX);
    final static  WPI_TalonFX frontRightMotor = new WPI_TalonFX(Constants.CANid.kFrontRightFX);
    final static  WPI_TalonFX frontLeftMotor = new WPI_TalonFX(Constants.CANid.kFrontLeftFX);
