80bb40bed5d6f5ced47395df3cf48ec44d03137e
Ramon Ledesma <rmon.led@gmail.com>
2023-08-25 19:35:22 -0700

    Auton configured to run for testing purposes

--- src/main/java/frc/robot/commands/Auto_one.java
+++ src/main/java/frc/robot/commands/Auto_one.java
@@ -1,6 +1,10 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.driveTrain;
+import frc.robot.subsystems.rotationSub;
+import frc.robot.subsystems.gripperSub;
+import frc.robot.subsystems.extendSub;
+import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 
@@ -9,7 +13,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class Auto_one extends SequentialCommandGroup {
   /** 
    *   * @param drive The drive subsystem this command will run on
-   * @return 
+   * 
    */
   public Auto_one(driveTrain drive){
     
