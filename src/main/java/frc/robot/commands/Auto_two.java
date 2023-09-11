c7d783bdb11bf312493ebcbabef0fc68620e72e4
Anthony Saysipaseuth <nickki456@gmail.com>
2023-09-11 16:31:58 -0700

    Crazy? I was crazy once. They locked me in a room, a rubber room, a rubber room with rats and rats make me crazy

---
+++ src/main/java/frc/robot/commands/Auto_two.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.driveTrain;
+import frc.robot.subsystems.rotationSub;
+import frc.robot.subsystems.gripperSub;
+import frc.robot.subsystems.extendSub;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+//sequential command groups go by steps. 
+//ex. step one first *then* step two and so on.
+public class Auto_two extends SequentialCommandGroup {
+  /** 
+   *   * @param drive The drive subsystem this command will run on
+   * 
+   */
+  public Auto_two(driveTrain drive){
+    drive.driveInDistance(3, 0.3);
+
+
+  }
+}
