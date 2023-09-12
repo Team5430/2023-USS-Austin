a3ec80528ac5e7dfbf6eb703be30112d64ca5c8a
Anthony Saysipaseuth <nickki456@gmail.com>
2023-09-12 06:48:01 -0700

    Crazy? I was crazy once.

--- src/main/java/frc/robot/commands/Auto_two.java
+++ src/main/java/frc/robot/commands/Auto_two.java
@@ -15,9 +15,24 @@ public class Auto_two extends SequentialCommandGroup {
    *   * @param drive The drive subsystem this command will run on
    * 
    */
-  public Auto_two(driveTrain drive){
-    drive.driveInDistance(3, 0.3);
+  public Auto_two(driveTrain drive) {
+    System.out.println("rotating..");
+    rotationSub.rotateinTime(1, 0.6);
 
+    System.out.println("Raising my arm!!!! RRAAAAHHHH!!!");
+    extendSub.extendTime(0.5, 0.6);
+
+    System.out.println("Oh lord, get a grip you're coming apart!!!");
+    gripperSub.gripinTime(0.8, 0.7);
+
+    System.out.println("Get this thing off my hand >:(");
+    gripperSub.releasingTime(0.8, 0.7);
+
+    System.out.println("Retracting..");
+    extendSub.retractTime(2, 0.6);
+
+    System.out.println("Reseting the grabbing point");
+    gripperSub.gripinTime(0.8, 0.7);
 
   }
 }
