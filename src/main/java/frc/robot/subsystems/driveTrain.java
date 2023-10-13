1562e1ba277031007ca6f7148a91bdbd6ef04c38
Ramon Ledesma <rmon.led@gmail.com>
2023-10-12 21:07:24 -0700

    Ability to drive backwards with sin function

--- src/main/java/frc/robot/subsystems/driveTrain.java
+++ src/main/java/frc/robot/subsystems/driveTrain.java
@@ -44,8 +44,13 @@ public class driveTrain extends SubsystemBase  {
 //drive with input  
     public void Drive(double left, double right){
     //sin used to smooth out the rate of acceleration
+    if(Constants.multiplier == -Math.abs(Constants.multiplier)){
+      leftGroup.set(Math.sin(left * Constants.multiplier)* -Math.sin(left * Constants.multiplier));
+      rightGroup.set(Math.sin(-right * Constants.multiplier) * -Math.sin(-right * Constants.multiplier)); 
+    }else{
       leftGroup.set(Math.sin(left * Constants.multiplier)* Math.sin(left * Constants.multiplier));
-      rightGroup.set(Math.sin(-right * Constants.multiplier) * Math.sin(-right * Constants.multiplier)); 
+      rightGroup.set(Math.sin(-right * Constants.multiplier) * Math.sin(-right * Constants.multiplier));
+    }
   }
 //Auto Drive
 
