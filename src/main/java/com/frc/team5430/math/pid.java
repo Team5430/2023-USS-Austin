d57b63c4411685a4657f858e4f6914bc385cf732
Anthony Saysipaseuth <nickki456@gmail.com>
2023-10-20 22:50:30 -0700

    Documentations created for PID

--- src/main/java/com/frc/team5430/math/pid.java
+++ src/main/java/com/frc/team5430/math/pid.java
@@ -9,9 +9,16 @@ import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Timer;
 
 public class pid implements Sendable {  
+  //kP - Proportional Term - to improve the transient response rise time and settling time
     private double kP_;
+
+    //kI - Integral term - Improve steady state response
     private double kI_;
+
+    // kI - Derivative Control - Improve the transient response by way of predicting error will occur in the future 
     private double kD_;
+
+    //Variable Creations
     private double error;
     private double lastError;
     private double errorSum;
@@ -23,6 +30,7 @@ public class pid implements Sendable {
  
     public pid(){}
 
+    //Variable Creations for kP,kI and kD
     public pid(double kP, double kI, double kD){
         this.kP_ = kP;
         this.kI_ = kI;
@@ -32,6 +40,7 @@ public class pid implements Sendable {
         SendableRegistry.addLW(this, "PID", instances);
         }
 
+        //Function to set PID 
     public void setPID(double kP, double kI, double kD){
         this.kP_ = kP;
         this.kI_ = kI;
@@ -42,38 +51,50 @@ public class pid implements Sendable {
         SendableRegistry.remove(this);
     }
 
+    //Function to get KP value
     public double getkP(){
         return kP_;
     }
-
+  //Function to get KP value
     public double getkI(){
         return kI_;
     }
-
+   //Function to get KD value
     public double getkD(){
         return kD_;
     }
+    //Function to set/update/change the KP Value
     public void setkP(double kP){
          kP_ = kP;
     }
-
+//Function to set/update/change the KI Value
     public void setkI(Double kI){
          kI_ = kI;
     }
-
+//Function to set/update/change the KD Value
     public void setkD(double kD){
          kD_ = kD;
     }
-    
+    //Function to set the the TimeStamp Value
     public void setTimetamp(){
         lastTimestamp = Timer.getFPGATimestamp();
     }
 
+    //Function of Calculation to set the setpoint value
   public double calc(double setpoint, double input){
+    //Motor error founded by setpoint minus the input value
     error = setpoint - input;
+
+    //get the timestamp minus the timestamp
     dT = Timer.getFPGATimestamp() - lastTimestamp;
+
+    //get the errorSum by having the error multiply by the dT but turn into positive
     errorSum += error * dT;
+
+    //errorRate be seen by having original error minus the lastError, then divide by the lastTimestamp
     errorRate = (error - lastError) / dT;
+
+    //add all of the values into a final PID Output
     PIDoutput = kP_ * error + kI_ * errorSum + kD_ * errorRate;
     //update values
     lastError = error;
@@ -81,6 +102,7 @@ public class pid implements Sendable {
     return PIDoutput;
   }
 
+  //reset error,lastError, and lastTimeStamp to 0
   public void reset(){
     error = 0;
     lastError = 0;
@@ -88,6 +110,8 @@ public class pid implements Sendable {
   }
 
   @Override
+
+  //SmartDashboard add-ons
   public void initSendable(SendableBuilder build){
     build.setSmartDashboardType("PID");
     build.addDoubleProperty("P", this::getkP, this::setkP);
